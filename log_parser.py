8ec4c2d6bcd9b2d2df51f1ff97e6dcdd14fcee19
Quang Vinh Ngô <vinhqngo5@gmail.com>
2020-12-02 10:16:14 +0700

    update sumup of task1

--- log_parser.py
+++ log_parser.py
@@ -12,10 +12,10 @@ FLAGS = flags.FLAGS
 # flags.DEFINE_string('typetopo', None, 'type of topology')
 flags.DEFINE_string('func', None, 'type of function')
 flags.DEFINE_boolean('t_test', False, 'if using t-test')
-flags.DEFINE_string('type', 'star', 'type of topo')
 
 def find_mean(file_path):
-    np_array = find_data(file_path)
+    good_infos = find_data(file_path)
+    np_array = np.array(good_infos)
     std = np.std(np_array)
     return sum(good_infos) / len(good_infos), std
 
@@ -34,7 +34,7 @@ def find_data(file_path):
 def independent_ttest(data1, data2, alpha=0.05):
     mean1, mean2 = mean(data1), mean(data2)
     se1, se2 = sem(data1), sem(data2)
-    sed = sqrt(se1**2.0 + se2**2.0)
+    sed = sqrt(se1 ** 2.0 + se2 ** 2.0)
     t_stat = (mean1 - mean2) / sed
     df = len(data1) + len(data2) - 2
     cv = t.ppf(1.0 - alpha, df)
@@ -45,43 +45,34 @@ def independent_ttest(data1, data2, alpha=0.05):
 
 def main(argv):
     swam_sizes = [128, 256, 512, 1024, 2048]
-    if (FLAGS.t_test == False):
-        star, ring, star_std, ring_std = [], [], [], []
-        for i in swam_sizes:
-            file_path = "/content/pyswarm/log/" + FLAGS.func + "_10dims_star_" + str(i) + "_log.txt"
-            mean, std = find_mean(file_path)
-            star.append(mean)
-            star_std.append(std)
-            file_path = "/content/pyswarm/log/" + FLAGS.func + "_10dims_ring_" + str(i) + "_log.txt"
-            mean, std = find_mean(file_path)
-            ring.append(mean)
-            ring_std.append(std)
+    star, ring, star_std, ring_std = [], [], [], []
+    for i in swam_sizes:
+        file_path = "/content/pyswarm/log/" + FLAGS.func + "_10dims_star_" + str(i) + "_log.txt"
+        mean, std = find_mean(file_path)
+        star.append(mean)
+        star_std.append(std)
+        file_path = "/content/pyswarm/log/" + FLAGS.func + "_10dims_ring_" + str(i) + "_log.txt"
+        mean, std = find_mean(file_path)
+        ring.append(mean)
+        ring_std.append(std)
 
-        print ("{:<15} {:<20}                               {:<15}".format('Popsize N', 'Star Topology', 'Ring Topology'))
-        for i in range(len(star)):
-            print ("{:<15} {:4f}({:4f})               {:<4f}({:<4f})".format(swam_sizes[i], star[i], star_std[i], ring[i], ring_std[i]))
-    else:
-        rastrigin, rosenbrock = [], []
+    print ("{:<15} {:<20}                               {:<15}".format('Popsize N', 'Star Topology', 'Ring Topology'))
+    for i in range(len(star)):
+        print ("{:<15} {:4f}({:4f})               {:<4f}({:<4f})".format(swam_sizes[i], star[i], star_std[i], ring[i], ring_std[i]))
+    if (FLAGS.t_test):
+        print(FLAGS.func)
         for i in swam_sizes:
-            file_path_1 = "/content/pyswarm/log/Rastrigin_10dims_" + FLAGS.type + "_" + str(i) + "_log.txt"
-            file_path_2 = "/content/pyswarm/log/Rosenbrock_10dims_" + FLAGS.type + "_" + str(i) + "_log.txt"
-            rastrigin = find_data(file_path_1)
-            rosenbrock = find_data(file_path_2)
+            file_path_1 = "/content/pyswarm/log/" + FLAGS.func + "_10dims_star_" + str(i) + "_log.txt"
+            file_path_2 = "/content/pyswarm/log/" + FLAGS.func + "_10dims_ring_" + str(i) + "_log.txt"
+            star = find_data(file_path_1)
+            ring = find_data(file_path_2)
             print('swarm_size: {}'.format(i))
-            t_stat, df, cv, p = independent_ttest(rastrigin, rosenbrock)
+            t_stat, df, cv, p = independent_ttest(star, ring)
             print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
-            # interpret via critical value
-            # if abs(t_stat) <= cv:
-            #     print('Accept null hypothesis that the means are equal.')
-            # else:
-            #     print('Reject the null hypothesis that the means are equal.')
-            
-
-            # interpret via p-value
             if p > 0.05:
-                print('Accept null hypothesis that the means are equal.')
+                print('Accept null hypothesis that the distribution of means are equal.')
             else:
-                print('Reject the null hypothesis that the means are equal.')
+                print('Reject the null hypothesis that the distribution of means are equal.')
             print('*'*65)
 
         
